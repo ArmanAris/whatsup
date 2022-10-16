63517c37231a531c548211804f97dfe1c6ca3c70
ArmanAris <114592462+ArmanAris@users.noreply.github.com>
2022-10-16 16:50:21 +0330

    master: add read database

--- app/src/main/java/com/aris/whatsapp/MainActivity.kt
+++ app/src/main/java/com/aris/whatsapp/MainActivity.kt
@@ -2,7 +2,11 @@ package com.aris.whatsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,7 +14,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var databaseA = FirebaseDatabase.getInstance()
-        var databaseref = databaseA.reference
+        var databaseref = databaseA.getReference("message").push()
         databaseref.setValue("Arman Aris")
+
+
+        databaseref.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                var data = snapshot.value as HashMap<String , Any>
+                data.get("Key")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(applicationContext, error.message, Toast.LENGTH_SHORT).show()
+            }
+
+        })
     }
 }
