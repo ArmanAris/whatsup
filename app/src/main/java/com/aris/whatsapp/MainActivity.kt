395f68a36adc274fc3a725cb22d2ea418da81856
ArmanAris <114592462+ArmanAris@users.noreply.github.com>
2022-10-18 15:48:37 +0330

    master: add library and color

--- app/src/main/java/com/aris/whatsapp/MainActivity.kt
+++ app/src/main/java/com/aris/whatsapp/MainActivity.kt
@@ -23,31 +23,6 @@ class MainActivity : AppCompatActivity() {
 
 
 
-        val Email1 = findViewById<EditText>(R.id.EmailAddress)
-        val password1 = findViewById<EditText>(R.id.Password)
-        val btn = findViewById<Button>(R.id.button)
-
-
-        val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
-
-        btn.setOnClickListener {
-            val Email = Email1.text.toString()
-            val password = password1.text.toString()
-            if (Email.trim().isNotEmpty() && password.trim().isNotEmpty()) {
-                mAuth.createUserWithEmailAndPassword(Email, password)
-                    .addOnCompleteListener { task: Task<AuthResult> ->
-                        if (task.isSuccessful) {
-                            val user: FirebaseUser = mAuth.currentUser!!
-                            Toast.makeText(this, user.email.toString(), Toast.LENGTH_SHORT).show()
-                        } else {
-                            Toast.makeText(this, "Errooooooor!!!!!", Toast.LENGTH_SHORT).show()
-                        }
-                    }
-
-            } else {
-                Toast.makeText(this, "اطلاعات را کامل کنید!!!!!!", Toast.LENGTH_SHORT).show()
-            }
-        }
 
 
     }
