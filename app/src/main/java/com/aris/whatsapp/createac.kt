3e6c33fba27d3f862878d7c290fe87962db2e9c1
ArmanAris <114592462+ArmanAris@users.noreply.github.com>
2022-10-20 10:23:50 +0330

    master: add page create account

---
+++ app/src/main/java/com/aris/whatsapp/createac.kt
@@ -0,0 +1,11 @@
+package com.aris.whatsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class createac : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_createac)
+    }
+}
